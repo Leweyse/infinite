262abe2c430d3fa3512cf536607040fbfcd408ce
Leweyse <darylcastro67@gmail.com>
2022-01-26 13:11:37 +0100

    imported contrast ratio helper

--- src/components/containers/Section/Section.js
+++ src/components/containers/Section/Section.js
@@ -1,6 +1,6 @@
 import { useEffect, useRef } from "react";
 
-import { GridRandomTemplate } from "../../../utils";
+import { GridRandomTemplate, ContrastRatio } from "../../../utils";
 import { Img } from "../../../store";
 
 import { Title, Figure, Nav } from "../../blocks";
